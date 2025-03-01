bc4b7cfe4b0f135924e195094f3aeb6f685f2331
Aman Amitabh <101924129+amanamitabh@users.noreply.github.com>
2025-03-01 23:35:37 +0530

    Update sample1.py

--- sample1.py
+++ sample1.py
@@ -1,10 +1,10 @@
 add(a, b):  #Implement by using parameterized function
-  return a+b
+
 
 subtract(): #Implement by using parameterized function
   pass
 
 multiply(a, b):
-  return a*b
+
 
 print("Hello")
